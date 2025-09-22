70134d7a61006933bf53732570fbd7a47767b1dc
joach <joach@KIM>
2025-09-22 15:00:19 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿static void Main()
+{
+    int alder;
+    Console.WriteLine("Vad är erat för eller efternamn?");
+    string namn = Console.ReadLine();
+    namn = char.ToUpper(namn[0]) + namn.Substring(1).ToLower();
+
+
+    if (namn == "Joachim" || namn == "Christiansson")
+    {
+        // joachim christiansson
+
+        alder = 33;
+        Console.WriteLine($"Välkommen {namn}! ");
+        Console.WriteLine($"Ock ni år {alder} gammal ");
+
+    }
+
+    if (namn == "Person2 förnamn" || namn == "Person2 efernamn")
+    {
+        //alder = Person2 ålder;
+        Console.WriteLine($"Välkommen {namn}! ");
+        Console.WriteLine($"Ock ni år {alder} gammal ");
+
+    }
+
+    if (namn == "Person3 förnamn" || namn == "Person3 efernamn")
+    {
+        //alder = Person3 ålder;
+        Console.WriteLine($"Välkommen {namn}! ");
+        Console.WriteLine($"Ock ni år {alder} gammal ");
+
+    }
+
+    if (namn == "Person4 förnamn" || namn == "Person4 efernamn")
+    {
+        //alder = Person4 ålder;
+        Console.WriteLine($"Välkommen {namn}! ");
+        Console.WriteLine($"Ock ni år {alder} gammal ");
+
+    }
+}
